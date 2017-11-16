b7ca2fd310d0ed713aa68791dcbcc8ab99eb6a40
elvis <poggi.elvis@gmail.com>
2017-11-16 14:05:47 +0100

    add Cell component

---
+++ src/components/Cell.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+let basicStyle = {
+  color: 'withe',
+  display: 'inline-block',
+  padding: '5px'
+}
+
+const Cell = (props = {}) => {
+  const { children, isHeader, hasColor, value } = props
+  const style = isHeader
+    ? {...basicStyle, background: 'gray'}
+    : hasColor
+    ? (value > 0)
+      ? {...basicStyle, background: 'rgba(157, 194, 74, 1.73)'}
+      : {...basicStyle, background: 'rgba(225, 86, 86, 1.52'}
+    : {...basicStyle, color: 'black'}
+
+  return (
+    <div style={style}> {children} </div>
+  )
+}
+
+export default Cell
